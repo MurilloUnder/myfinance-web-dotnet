fa81fe751f9ff311cc38423bd70ecccbfd39c4b3
MurilloUndergyn <124592291+MurilloUnder@users.noreply.github.com>
2023-02-21 11:07:26 -0300

    Atualização da DOC

--- docs/myfinance_script.sql
+++ docs/myfinance_script.sql
@@ -8,7 +8,7 @@ tipo char(1) not null,
 primary key (id)
 );
 
-create table trasancao(
+create table transacao(
 id int identity(1,1)not null,
 data datetime not null,
 valor decimal(9,2),
@@ -18,19 +18,57 @@ primary key(id),
 foreign key(planocontaid) references planoconta(id)
 );
 
-SELECT id, data,valor, historico, planocontaid FROM transacao
+SELECT * FROM planoconta
 
 
+insert into planoconta(descricao, tipo)
+values('Combustível', 'D')
+
+insert into transacao(data, valor, historico, planocontaid)
+values('20230102 15:00', 562.45, 'IPTU', 10)
 
 
+--Todas as transações de Despesas no mês de Janeiro
+SELECT t.data, t.valor, p.descricao 
+FROM transacao t
+inner join planoconta p on t.planocontaid = p.id
+where p.tipo = 'D' and t.data >='20230101' and t.data <='20230131'
 
+--Todas as transações maiores que 200 reais
+select * from transacao where valor > 200
 
+--Somatoria de transações de receitas e despesas de todo o período
+select sum(valor) as total_despesas from transacao t join planoconta p on p.id = t.planocontaid
+where p.tipo = 'D'
+union all
+select sum(valor) as total_receitas from transacao t join planoconta p on p.id = t.planocontaid
+where p.tipo = 'R'
 
+select 
+  d.total_despesas, 
+  r.total_receitas 
+from
+  (select sum(valor) as total_despesas 
+  from transacao t join planoconta p on p.id = t.planocontaid
+  where p.tipo = 'D') as d,
+  (select sum(valor) as total_receitas 
+from 
+  transacao t join planoconta p on p.id = t.planocontaid
+  where p.tipo = 'R') as r
 
+  --Média de transações por mês
+  select 
+  avg(valor) as media,
+  MONTH(data) as mes 
+  from transacao
+  group by month(data)
 
+ select year(getdate())
 
-
-
+--Transações e seus itens de plano de contas
+select t.id, t.data, t.valor, p.descricao, p.tipo
+from transacao t join planoconta p
+on t.planocontaid = p.id
 
 
 --SQL
